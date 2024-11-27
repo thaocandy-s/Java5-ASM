75c622b4256e1ab36a71f95afc1ce8409272f9fa
Nguyen Thu Thao <166482816+thaocandy-s@users.noreply.github.com>
2024-11-27 17:52:38 +0700

    update BanHangService

--- menfashion/src/main/java/poly/thao/menfashion/service/BanHangService.java
+++ menfashion/src/main/java/poly/thao/menfashion/service/BanHangService.java
@@ -1,5 +1,6 @@
 package poly.thao.menfashion.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import poly.thao.menfashion.entity.HoaDon;
@@ -18,11 +19,62 @@ import java.util.Map;
 @Service
 public class BanHangService {
 
+    @Autowired
+    private KhachHangService khachHangService;
+
+    @Autowired
+    private SanPhamChiTietService sanPhamChiTietService;
+
+    @Autowired
+    private HoaDonService hoaDonService;
+
+    @Autowired
+    private HoaDonChiTietService hoaDonChiTietService;
+
     public Double getAmount(Map<SanPhamChiTiet, Integer> mapSP){
         Double amount = 0D;
         for (Map.Entry<SanPhamChiTiet, Integer> entry: mapSP.entrySet()){
             amount += entry.getKey().getDonGia()*entry.getValue();
         }
         return amount;
+    }
+
+
+    @Transactional(rollbackFor = Exception.class)
+    public ResponseObject<String> pay(Cart cart) {
+        try {
+            NhanVien nv = AppService.currentUser;
+            KhachHang kh = khachHangService.findById(cart.idKhachHang).data;
+
+            HoaDon hoaDon = new HoaDon();
+            hoaDon.setNhanVien(nv);
+            hoaDon.setKhachHang(kh);
+            hoaDon.setTrangThai(EntityStatus.ACTIVE);
+            ResponseObject<HoaDon> savedHoadon = hoaDonService.add(hoaDon);
+            if (savedHoadon.isHasError) {
+                throw new RuntimeException(savedHoadon.message);
+            }
+
+            for (Map.Entry<Integer, Integer> sp : cart.mapSanPham.entrySet()) {
+                HoaDonChiTiet hoaDonChiTiet = new HoaDonChiTiet();
+                hoaDonChiTiet.setHoaDon(savedHoadon.data);
+                SanPhamChiTiet spct = sanPhamChiTietService.findById(sp.getKey()).data;
+                hoaDonChiTiet.setSanPhamChiTiet(spct);
+                hoaDonChiTiet.setDonGia(spct.getDonGia());
+                hoaDonChiTiet.setSoLuong(sp.getValue());
+                hoaDonChiTiet.setTrangThai(EntityStatus.ACTIVE);
+                ResponseObject<HoaDonChiTiet> savedHDCT = hoaDonChiTietService.add(hoaDonChiTiet);
+
+                if (savedHDCT.isHasError) {
+                    throw new RuntimeException(savedHDCT.message);
+                }
+                spct.setSoLuong(spct.getSoLuong() - sp.getValue());
+                sanPhamChiTietService.update(spct);
+            }
+
+            return new ResponseObject<>(false, "OK", "Thanh toán thành công.");
+        } catch (Exception ex) {
+            return new ResponseObject<>(true, null, "Lỗi: " + ex.getMessage());
+        }
     }
 }
